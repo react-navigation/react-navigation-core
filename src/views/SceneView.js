c91417515b3114cb9593c1e15e2fb0c3444ccd13
gazoudoudou <gaspardd@MacBook-Pro-de-Gaspard.local>
2018-11-11 17:23:37 +0100

    Add nodeTag navigation param when mount a sceneview

--- src/views/SceneView.js
+++ src/views/SceneView.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { findNodeHandle } from 'react-native';
 import NavigationContext from './NavigationContext';
 
 export default class SceneView extends React.PureComponent {
+  componentDidMount() {
+    this.props.navigation.setParams({ nodeTag: findNodeHandle(this) });
+  }
+
   render() {
     const { screenProps, component: Component, navigation } = this.props;
     return (
